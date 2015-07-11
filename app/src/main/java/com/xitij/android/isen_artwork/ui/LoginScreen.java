6340626e42ad4ffce47a4b15c31e4d89dcd40c90
Dhruvil <dd.droid@live.com>
2015-07-11 16:18:54 +0530

    n
    n

--- app/src/main/java/com/xitij/android/isen_artwork/ui/LoginScreen.java
+++ app/src/main/java/com/xitij/android/isen_artwork/ui/LoginScreen.java
@@ -13,6 +13,7 @@ public class LoginScreen extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        //Hello
     }
 
 }
